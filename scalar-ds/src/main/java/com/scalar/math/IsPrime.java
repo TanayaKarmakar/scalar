5d2ab8aa81634d9e9bdba634c0b7a1bcb1004e9f
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2022-01-05 09:36:43 +0530

    assignment + homework for week1

---
+++ scalar-ds/src/main/java/com/scalar/math/IsPrime.java
@@ -0,0 +1,32 @@
+package com.scalar.math;
+
+import java.util.Scanner;
+
+/**
+ * @author t0k02w6 on 05/01/22
+ * @project scalar-ds
+ */
+public class IsPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        int val = (int)Math.sqrt(num);
+
+        int i = 2;
+        boolean isPrime = true;
+        while(i <= val) {
+            if(num % i == 0) {
+                System.out.println("NO");
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+
+        if(isPrime)
+            System.out.println("YES");
+        scanner.close();
+    }
+}
---
+++ scalar-ds/src/main/java/com/scalar/math/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.scalar.math;
+
+import java.util.Scanner;
+
+/**
+ * @author t0k02w6 on 05/01/22
+ * @project scalar-ds
+ */
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+
+        scanner.close();
+    }
+}
