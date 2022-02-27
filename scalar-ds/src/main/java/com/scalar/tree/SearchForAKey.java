62e6cb822436f62082a92e42977987786a7a95c8
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2022-02-27 09:15:25 +0530

    code

---
+++ scalar-ds/src/main/java/com/scalar/tree/SearchForAKey.java
@@ -0,0 +1,23 @@
+package com.scalar.tree;
+
+import com.scalar.common.TreeNode;
+
+/**
+ * @author t0k02w6 on 27/02/22
+ * @project scalar-ds
+ */
+public class SearchForAKey {
+    private static boolean search(TreeNode root, int key) {
+        if(root == null)
+            return false;
+        if(root.val == key)
+            return true;
+        boolean isFoundAtLeft = search(root.left, key);
+        boolean isFoundAtRight = search(root.right, key);
+        return isFoundAtLeft || isFoundAtRight;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
